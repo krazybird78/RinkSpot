60416fc5e80d75b89e7283d6d2fff46017bd679a
krazybird78 <doyon.sebas@gmail.com>
2026-01-17 14:03:46 -0500

    feat: Add Microsoft Clarity script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Outfit, Press_Start_2P } from "next/font/google"; // Updated font imports
 import "./globals.css";
 
@@ -37,6 +38,15 @@ export default function RootLayout({
         className={`${outfit.variable} ${pressStart2P.variable} antialiased`}
       >
         {children}
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v2ztlbhjcu");
+          `}
+        </Script>
       </body>
     </html>
   );
